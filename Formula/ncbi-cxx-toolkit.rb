883927b7136e7ccb06b60a6e737a28cac6efb2c9
actapia <13249250+actapia@users.noreply.github.com>
2026-01-21 07:14:24 +0000

    Update ncbi-cxx-toolkit formula bottle section

--- Formula/ncbi-cxx-toolkit.rb
+++ Formula/ncbi-cxx-toolkit.rb
@@ -11,10 +11,10 @@ class NcbiCxxToolkit < Formula
 
   bottle do
     root_url "https://cs.uky.edu/~acta225/brew"
-    rebuild 2
-    sha256 cellar: :any, arm64_tahoe:   "acc1fc6a2c28ad5e954646685e1d8895ccbe4c26c225237ea5ec19cad62d667d"
-    sha256 cellar: :any, arm64_sequoia: "f743924442cfaf88cceabc8672680eab009c62316be1f54fb761aa378253c094"
-    sha256 cellar: :any, sequoia:       "78ed748470a1719d2f552cc44ecaf444dd0ddeced9d82c9a91ee33febf6d52ad"
+    rebuild 3
+    sha256 cellar: :any, arm64_tahoe:   "323da4f7c97bbb5e0a7dd2922bca54747ecd9c646a549a21cd6f45d330e27df7"
+    sha256 cellar: :any, arm64_sequoia: "31a0d42465728fe121f88b1a367d64f510db463534eb0c3147bd5f074753b1fd"
+    sha256 cellar: :any, sequoia:       "321913b5cdd98bfb61cab4343571d0adb15c7f7b93b2714edbe3abf758d82c83"
   end
 
   keg_only "Includes many files that would pollute bin, lib, and so on, and this
